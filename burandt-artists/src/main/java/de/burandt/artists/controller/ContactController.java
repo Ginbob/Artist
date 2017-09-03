75f4479301801968242c8f03d64a5f65ccde8069
henning <henning.nobbe@innoq.com>
2017-09-03 19:28:14 +0200

    Email

--- burandt-artists/src/main/java/de/burandt/artists/controller/ContactController.java
+++ burandt-artists/src/main/java/de/burandt/artists/controller/ContactController.java
@@ -1,15 +1,5 @@
 package de.burandt.artists.controller;
 
-import javax.mail.Message;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.MailException;
-import org.springframework.mail.MailSender;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,33 +8,16 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class ContactController {
 	
-	@Autowired
-	private JavaMailSender mailSender;
-	
 	@PostMapping(path="/sendMessage")
 	public ModelAndView sendMessageFromContact(@RequestParam(name="c-email") final String email,
 			@RequestParam(name="c-reason") final String contactReason,
 			@RequestParam(name="c-message") final String contactMessage) {
 		ModelAndView model = new ModelAndView("messageSent");
 		
-		MimeMessagePreparator message = new MimeMessagePreparator() {
-			
-			@Override
-			public void prepare(MimeMessage mimeMessage) throws Exception {
-				mimeMessage.setRecipient(Message.RecipientType.TO, new InternetAddress("henning.nobbe@gmail.com"));
-				mimeMessage.setFrom(new InternetAddress(email));
-				mimeMessage.setText(contactMessage);
-				mimeMessage.setSubject(contactReason);
-			}
-		};
-		
-		try {
-			mailSender.send(message);
-		}
-	    catch (MailException ex) {
-	        // simply log it and go on...
-	        System.err.println(ex.getMessage());
-	    }
+		StringBuffer msg = new StringBuffer(email).append(" schrieb folgende Nachricht über das Kontaktformular:");
+		msg.append("\n\n");
+		msg.append(contactMessage);
+		ContactUtils.sendContactEmail(msg.toString(), contactReason);
 		
 		return model;
 	}
