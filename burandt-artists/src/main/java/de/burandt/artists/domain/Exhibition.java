e801348cc1dfd254c3cdd3e2c8df1e28db1660f4
bmakowe <bjoern.makowe@innoq.com>
2017-10-10 13:51:57 +0200

    Ich hab keine Ahnung mehr

--- burandt-artists/src/main/java/de/burandt/artists/domain/Exhibition.java
+++ burandt-artists/src/main/java/de/burandt/artists/domain/Exhibition.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.sql.Date;
 
 @Entity
 public class Exhibition {
@@ -11,30 +12,39 @@ public class Exhibition {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer id;
-	private String exhibitionDate;
+	private Date exhibitionStartDate;
+	private Date exhibitionEndDate;
 	private String description;
 
-	public Integer getId() {
-		return id;
-	}
+    public Integer getId() {
+        return id;
+    }
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-	public String getExhibitionDate() {
-		return exhibitionDate;
-	}
+    public Date getExhibitionStartDate() {
+        return exhibitionStartDate;
+    }
 
-	public void setExhibitionDate(String exhibitionDate) {
-		this.exhibitionDate = exhibitionDate;
-	}
+    public void setExhibitionStartDate(Date exhibitionStartDate) {
+        this.exhibitionStartDate = exhibitionStartDate;
+    }
 
-	public String getDescription() {
-		return description;
-	}
+    public Date getExhibitionEndDate() {
+        return exhibitionEndDate;
+    }
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+    public void setExhibitionEndDate(Date exhibitionEndDate) {
+        this.exhibitionEndDate = exhibitionEndDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
